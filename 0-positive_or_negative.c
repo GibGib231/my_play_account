d81293d676089ffbbba364c76d64383f15742dc3
Amb Gibson Smith <www.1gibson1smith@gmail.com>
2025-04-08 17:42:59 +0000

    this code have been updated from my local machine

--- 0-positive_or_negative.c
+++ 0-positive_or_negative.c
@@ -1,21 +1,23 @@
 #include<stdio.h>
 int main(void)
 {
-int num1 = 20;
+int num1 = 0;
 
-if (num1 < 18)
+if (num1 < 0)
+	// just make a few changese in the code 
+	// so it's updated now
 	// this is to understand the if, else if and else condition
 		{
-		printf("%d that is not Amb Smith age\n", num1);
+		printf("%d that is not a positive number\n", num1);
 		}
-else if(num1 > 18)
+else if(num1 > 0)
 {
 
-	printf("%d is too higher to be Amb Smith age\n", num1);
+	printf("%d is a positive number\n but greater then zero in this code\n", num1);
 }
 else
 {
-	printf("%d is the age of the Amb\n", num1);
+	printf("%d is equal to zero \n", num1);
 }
 
 }
