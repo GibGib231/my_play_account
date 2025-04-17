891b39a2f289fd188caf9ee7709a9084b8153434
Amb Gibson Smith <www.1gibson1smith@gmail.com>
2025-04-17 09:42:26 +0000

    this is a imet game, if the user input num between 2 - 6 not more or less will get the time of that num

---
+++ game_time_table.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main()
+{
+
+	int user;
+
+	printf("Enter a number less then 6 and not 1 >> ");
+		scanf("%d", &user);
+	if(user == 2){
+	
+		for(int g = 1; g <= 12; g++)
+			printf("%d x %d = %d\n", 2, g, 2 * g);
+	}
+	else if(user == 3){
+
+                for(int g = 1; g <= 12; g++)
+                        printf("%d x %d = %d\n", 3, g, 3 * g);
+        }
+	   else if(user == 4){
+
+                for(int g = 1; g <= 12; g++)
+                        printf("%d x %d = %d\n", 4, g, 4 * g);
+        }   else if(user == 5){
+
+                for(int g = 1; g <= 12; g++)
+                        printf("%d x %d = %d\n", 5, g, 5 * g);
+        }  else if(user == 6){
+
+                for(int g = 1; g <= 12; g++)
+                        printf("%d x %d = %d\n", 6, g, 6 * g);
+        }
+
+
+
+	else
+		printf("we do't have that one in our code base please.\n");
+	return(0);
+}
